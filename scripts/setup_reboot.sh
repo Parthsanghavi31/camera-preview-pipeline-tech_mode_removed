ff5aa0f787eefa6920aa719f90548c37bf05c408
Parth <parths@viatouchmedia.com>
2026-01-19 17:23:11 -0500

    Added cron job for removing the content of post_archive and archive every night

--- scripts/setup_reboot.sh
+++ scripts/setup_reboot.sh
@@ -12,10 +12,14 @@ echo "Setting up cron job for daily reboot at 2 AM in root's crontab..."
 temp_cron=$(mktemp)
 sudo crontab -l > "$temp_cron"
 cron_job="0 2 * * * /sbin/reboot"
-if ! grep -Fxq "$cron_job" "$temp_cron"; then
+cleanup_job="0 1 * * 0 rm -rf /home/parth/camera-preview-pipeline-tech_mode_removed/post_archive/* /home/parth/camera-preview-pipeline-tech_mode_removed/archive/*"
+
+if ! grep -Fxq "$cron_job" "$cleanup_job" "$temp_cron"; then
     echo "$cron_job" >> "$temp_cron"
+    echo "$cleanup_job" >> "$temp_cron"
 fi
 sudo crontab "$temp_cron"
 rm "$temp_cron"
 sudo systemctl restart cron
 echo "Configuration complete."
+
